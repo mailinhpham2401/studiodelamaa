197c233b68b5e02c9537e7bdab363f2820cd85e0
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-03-21 21:12:37 +0100

    Init add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'myweb';
+
+  clicked(){
+    var background= document.getElementById('background');
+    return background;
+  }
 }
+ 
